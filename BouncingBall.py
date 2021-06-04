e9cf8b5936c64647d803087a71bad7200092ace3
Wengraf Designs <64872799+JoshuaWengraf@users.noreply.github.com>
2021-06-04 21:20:50 +0100

    Add files via upload

--- BouncingBall.py
+++ BouncingBall.py
@@ -2,6 +2,8 @@
 """
 Created on Thu Jun  3 10:28:00 2021
 
+Bouncing ball animation. 
+
 @author: jweng
 """
 
@@ -9,13 +11,19 @@ import turtle
 import time
 from PIL import Image
 
-def save(counter=[1]):
-    turtle.getcanvas().postscript(file = "BouncingBall{0:03d}.eps".format(counter[0]))
+
+def save(counter = [1]):
+    turtle.getcanvas().postscript(file = "BouncingBall{0:03d}.eps".format(counter[0])) 
+    turtle_img = Image.open("BouncingBall{0:03d}.eps".format(counter[0]))
+    turtle_img.save("BouncingBall{0:03d}.png".format(counter[0]), "png")
+    
     counter[0] += 1
-    
     print(counter)
+    turtle.ontimer(save, int(1000/FRAMES_PER_SECOND))
     
-    turtle.ontimer(save, int(1000/30))
+
+FRAMES_PER_SECOND = 30
+
 
 window = turtle.Screen()
 window.bgcolor("white")
@@ -24,51 +32,41 @@ window.title("Bouncing Ball")
 ball = turtle.Turtle()
 ball.shape("circle")
 
-ball.penup()
-ball.speed(0)
+ball.penup() # no drawn line
+ball.speed(0) # set initial speed
 
-ball.goto(0, 150)
+y = 150 # initial position
+v = -4 # initial speed
+g = -0.15 # gravitational constant
 
-ball.dy = -2
-
-FRAMES_PER_SECOND = 15
-
-gravity = 0.15
-
-timelimit = 5.3
-start_time = time.time()
+timelimit = 60
 
 t = 0
 
-
-def draw():
-    
-    ball.dy -= gravity
-    ball.sety(ball.ycor() + ball.dy)
-        
-    if ball.ycor() < -100:
-        ball.dy *= -1
-        
 save()
+
+start_time = time.time()
 
 running = True
 while True:
-    draw()
+    v = v + g*t
+    y = y + g*t**2 + v*t
+    
+    ball.sety(y)
+        
+    if y < -100:
+        v *= -1
     
     elapsed_time = time.time() - start_time
-    if elapsed_time >= 6:
+    
+    if elapsed_time >= timelimit:
         print('animation finished')
         break
+    
+    t = t + 0.01
 
+turtle.clearscreen()
 turtle.bye()
 
-for ctr in range(1, 10):
-    turtle_img = Image.open("iceland00" + str(ctr) + ".eps")
-    turtle_img.save("iceland00" + str(ctr) + ".png", "png")
-
-for ctr in range(10, 100):
-    turtle_img = Image.open("iceland0" + str(ctr) + ".eps")
-    turtle_img.save("iceland0" + str(ctr) + ".png", "png")
-    
 
         
